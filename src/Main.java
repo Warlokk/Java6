9c33c4488a0e3080eeca7cd5a7c70c085ca0c206
Yuriy Nikolaev <warlokk83@gmail.com>
2021-01-18 22:50:41 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int transaction = 300;
+        int bonusCost = 100;
+        int minTransactionForBonus = 1000;
+        int bonus;
+        if (transaction>=minTransactionForBonus) {
+            bonus = transaction/bonusCost;
+        } else bonus = 0;
+        balance = balance + transaction + bonus;
+        System.out.println("Начислено бонусов: " + bonus);
+        System.out.println("Итоговый баланс: " + balance);
+    }
+}
